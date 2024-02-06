88b76d9dca8073eef99aa51fcd8f1d4314ccae0b
alvarorichard <alvaro10048@protonmail.com>
2024-02-06 08:13:21 -0300

    The aforementioned loop

--- java/Zephylix/src/zephylix/Scanner.java
+++ java/Zephylix/src/zephylix/Scanner.java
@@ -14,4 +14,16 @@ public class Scanner {
     Scanner(String source) {
         this.source = source;
     }
+
+    List<Token> scanTokens() {
+        int start = 0;
+        int current = 0;
+        int line = 1;
+        while (current < source.length()) {
+            start = current;
+            scanToken();
+        }
+        tokens.add(new Token(EOF, "", null, line));
+        return tokens;
+    }
 }
