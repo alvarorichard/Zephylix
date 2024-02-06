a15299a16ca85a6d939127fc6b2dcee9582938fe
alvarorichard <alvaro10048@protonmail.com>
2024-02-06 08:18:55 -0300

    Scanner Token

--- java/Zephylix/src/zephylix/Scanner.java
+++ java/Zephylix/src/zephylix/Scanner.java
@@ -10,6 +10,9 @@ import static zephylix.TokenType.*;
 public class Scanner {
     private final String source;
     private final List<Token> tokens = new ArrayList<>();
+    private int start = 0;
+    private int current = 0;
+    private int line = 1;
 
     Scanner(String source) {
         this.source = source;
@@ -25,5 +28,25 @@ public class Scanner {
         }
         tokens.add(new Token(EOF, "", null, line));
         return tokens;
+    }
+
+    private boolean isAtEnd() {
+        return current >= source.length();
+    }
+
+    private void scanToken(){
+        char c = advance();
+        switch (c){
+            case '(': addToken(LEFT_PAREN); break;
+            case ')': addToken(RIGHT_PAREN); break;
+            case '{': addToken(LEFT_BRACE); break;
+            case '}': addToken(RIGHT_BRACE); break;
+            case ',': addToken(COMMA); break;
+            case '.': addToken(DOT); break;
+            case '-': addToken(MINUS); break;
+            case '+': addToken(PLUS); break;
+            case ';': addToken(SEMICOLON); break;
+            case '*': addToken(STAR); break;
+        }
     }
 }
