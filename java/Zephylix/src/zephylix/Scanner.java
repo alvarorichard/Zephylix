942a7ec02fa42401d84231ee6350d15fc7481a33
alvarorichard <alvaro10048@protonmail.com>
2024-02-10 01:15:56 -0300

    identifier’s lexeme

--- java/Zephylix/src/zephylix/Scanner.java
+++ java/Zephylix/src/zephylix/Scanner.java
@@ -84,8 +84,31 @@ public class Scanner {
                 break;
 
         }
-        // continuar em Number literals
+        
     }
+
+    private static final Map<String, TokenType> keywords;
+
+    static {
+        keywords = new HashMap<>();
+        keywords.put("and",    AND);
+        keywords.put("class",  CLASS);
+        keywords.put("else",   ELSE);
+        keywords.put("false",  FALSE);
+        keywords.put("for",    FOR);
+        keywords.put("fun",    FUN);
+        keywords.put("if",     IF);
+        keywords.put("nil",    NIL);
+        keywords.put("or",     OR);
+        keywords.put("print",  PRINT);
+        keywords.put("return", RETURN);
+        keywords.put("super",  SUPER);
+        keywords.put("this",   THIS);
+        keywords.put("true",   TRUE);
+        keywords.put("var",    VAR);
+        keywords.put("while",  WHILE);
+    }
+
     private void string(){
         while (peek() != '"' && !isAtEnd()){
             if (peek() == '\n') line++;
@@ -129,8 +152,13 @@ public class Scanner {
 
     private void identifier() {
         while (isAlphaNumeric(peek())) advance();
+      //modifictions
+        String text = source.substring(start, current);
+        TokenType type = keywords.get(text);
+        if (type == null) type = IDENTIFIER;
+        addToken(type);
 
-        addToken(IDENTIFIER);
+       // addToken(IDENTIFIER);
     }
 
     private boolean isAlpha(char c) {
