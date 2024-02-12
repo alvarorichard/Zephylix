da071e2af30d4c53b3ecdc17ca6621ce7af3f09d
alvarorichard <alvaro10048@protonmail.com>
2024-02-12 01:40:23 -0300

    add  Visitor pattern

--- java/Zephylix/src/tool/GenerateAST.java
+++ java/Zephylix/src/tool/GenerateAST.java
@@ -62,16 +62,29 @@ public class GenerateAST {
     }
 
     abstract class Pastry {
+        abstract void accept(PastryVisitor visitor);
 
     }
 
     class Beignet extends Pastry {
-
+        @Override
+        void accept(PastryVisitor visitor) {
+            visitor.visitBeignet(this);
+        }
     }
 
     class Cruller extends Pastry {
+        @Override
+        void accept(PastryVisitor visitor) {
+            visitor.visitCruller(this);
+        }
 
     }
+    interface   PastryVisitor  {
+        void   visitBeignet  (  Beignet   beignet  );
+        void   visitCruller  (  Cruller   cruller  );
+    }
+
 
 
 }
