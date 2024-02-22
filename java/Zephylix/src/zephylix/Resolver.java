86c9a690f1690c830436f84c4c27b38f7131648f
alvarorichard <alvaro10048@protonmaill.com>
2024-02-22 02:02:30 -0300

    add two statements for the branches

--- java/Zephylix/src/zephylix/Resolver.java
+++ java/Zephylix/src/zephylix/Resolver.java
@@ -22,6 +22,34 @@ public class Resolver implements Expr.Visitor<Void>,Stmt.Visitor {
         return null;
     }
 
+    @Override
+    public Void visitExpressionStmt(Stmt.Expression stmt) {
+        resolve(stmt.expression);
+        return null;
+    }
+
+    @Override
+    public Void visitFunctionStmt(Stmt.Function stmt) {
+        declare(stmt.name);
+        define(stmt.name);
+
+        resolveFunction(stmt);
+        return null;
+    }
+
+    //dia 22/02/24
+
+    //An if statement has an expression for its condition and one or two statements for the branches.
+
+    @Override
+    public Void visitIfStmt(Stmt.If stmt) {
+        resolve(stmt.condition);
+        resolve(stmt.thenBranch);
+        if (stmt.elseBranch != null) resolve(stmt.elseBranch);
+        return null;
+    }
+
+
     void resolve(List<Stmt> statements){
         for (Stmt statement : statements){
             resolve(statement);
@@ -30,6 +58,17 @@ public class Resolver implements Expr.Visitor<Void>,Stmt.Visitor {
     private void resolve(Stmt statement){
         stmt.accept(this);
     }
+
+    private void resolveFunction(Stmt.Function function) {
+        beginScope();
+        for (Token param : function.params) {
+            declare(param);
+            define(param);
+        }
+        resolve(function.body);
+        endScope();
+    }
+
 
     private  void resolve(Expr expr){
         expr.accept(this);
@@ -43,6 +82,29 @@ public class Resolver implements Expr.Visitor<Void>,Stmt.Visitor {
         scopes.pop();
     }
 
+    private void declare(Token name){
+        if (scopes.isEmpty()) return;
+        Map<String, Boolean> scope = scopes.peek();
+        if (scope.containsKey(name.lexeme)){
+            lox.error(name, "Variable with this name already declared in this scope.");
+        }
+        scope.put(name.lexeme, false);
+    }
+
+    private void define(Token name) {
+        if (scopes.isEmpty()) return;
+        scopes.peek().put(name.lexeme, true);
+    }
+
+    private void resolveLocal(Expr expr, Token name) {
+        for (int i = scopes.size() - 1; i >= 0; i--) {
+            if (scopes.get(i).containsKey(name.lexeme)) {
+                interpreter.resolve(expr, scopes.size() - 1 - i);
+                return;
+            }
+        }
+    }
+
     @Override
     public Void visitVarStmt(Stmt.Var stmt){
         declare(stmt.name);
@@ -53,16 +115,25 @@ public class Resolver implements Expr.Visitor<Void>,Stmt.Visitor {
         return null;
     }
 
-    private void declare(Token name){
-        if (scopes.isEmpty()) return;
-        Map<String, Boolean> scope = scopes.peek();
-        if (scope.containsKey(name.lexeme)){
-            lox.error(name, "Variable with this name already declared in this scope.");
-        }
-        scope.put(name.lexeme, false);
+    @Override
+    public Void visitAssignExpr(Expr.Assign expr) {
+        resolve(expr.value);
+        resolveLocal(expr, expr.name);
+        return null;
     }
 
-    
+    @Override
+    public Void visitVariableExpr(Expr.Variable expr) {
+        if (!scopes.isEmpty() &&
+                scopes.peek().get(expr.name.lexeme) == Boolean.FALSE) {
+            Lox.error(expr.name,
+                    "Can't read local variable in its own initializer.");
+        }
+
+        resolveLocal(expr, expr.name);
+        return null;
+    }
+
 
 
 }
