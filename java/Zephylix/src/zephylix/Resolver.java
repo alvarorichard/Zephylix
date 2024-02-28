5b5930779783fdc7a3cb9a3c336dbdb113da6075
alvarorichard <alvaro10048@protonmail.com>
2024-02-28 11:19:25 -0300

    static analysis does no control flow or short-circuiting

--- java/Zephylix/src/zephylix/Resolver.java
+++ java/Zephylix/src/zephylix/Resolver.java
@@ -160,6 +160,39 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor {
         return null;
     }
 
+    @Override
+    public Void visitCallExpr(Expr.Call expr) {
+        resolve(expr.callee);
+        for (Expr argument : expr.arguments) {
+            resolve(argument);
+        }
+        return null;
+    }
+
+    @Override
+    public Void visitGroupingExpr(Expr.Grouping expr) {
+        resolve(expr.expression);
+        return null;
+    }
+
+    @Override
+    public Void visitLiteralExpr(Expr.Literal expr) {
+        return null;
+    }
+
+    @Override
+    public Void visitLogicalExpr(Expr.Logical expr) {
+        resolve(expr.left);
+        resolve(expr.right);
+        return null;
+    }
+
+
+    @Override
+    public Void visitUnaryExpr(Expr.Unary expr) {
+        resolve(expr.right);
+        return null;
+    }
 
     @Override
     public Void visitVariableExpr(Expr.Variable expr) {
