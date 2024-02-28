4899cc5cb3e47523aa264c66639b0a47da950a3d
alvarorichard <alvaro10048@protonmail.com>
2024-02-28 11:26:54 -0300

    That delegates

--- java/Zephylix/src/zephylix/Interpreter.java
+++ java/Zephylix/src/zephylix/Interpreter.java
@@ -85,7 +85,16 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
     @Override
     public Object visitVariableExpr(Expr.Variable expr) {
-        return environment.get(expr.name);
+        return lookUpVariable(expr.name, expr);
+    }
+
+    private Object lookUpVariable(Token name, Expr expr) {
+        Integer distance = locals.get(expr);
+        if (distance != null) {
+            return environment.getAt(distance, name.lexeme);
+        } else {
+            return globals.get(name);
+        }
     }
 
     private void checkNumberOperand(Token operator, Object operand) {
