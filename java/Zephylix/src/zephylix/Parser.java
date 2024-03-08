3df20d43f876c0faf57b17c9db96a5257796681c
alvarorichard <alvaro10048@protonmail.com>
2024-03-08 19:46:50 -0300

    The new parser code

--- java/Zephylix/src/zephylix/Parser.java
+++ java/Zephylix/src/zephylix/Parser.java
@@ -63,6 +63,13 @@ public class Parser {
 
     private Stmt classDeclaration() {
         Token name = consume(IDENTIFIER, "Expect class name.");
+
+        Expr.Variable superclass = null;
+        if (match(LESS)) {
+            consume(IDENTIFIER, "Expect superclass name.");
+            superclass = new Expr.Variable(previous());
+        }
+
         consume(LEFT_BRACE, "Expect '{' before class body.");
 
         List<Stmt.Function> methods = new ArrayList<>();
@@ -72,7 +79,7 @@ public class Parser {
 
         consume(RIGHT_BRACE, "Expect '}' after class body.");
 
-        return new Stmt.Class(name, methods);
+        return new Stmt.Class(name, superclass, methods);
     }
 
     private Stmt statement() {
