dd3631d6ece0a94d85e60cc7ac58a6899507b9cd
alvarorichard <alvaro10048@protonmail.com>
2024-04-01 20:04:29 -0300

    fix issues in LoxInstance

--- java/Zephylix/src/zephylix/LoxFunction.java
+++ java/Zephylix/src/zephylix/LoxFunction.java
@@ -1,54 +1,91 @@
+//> Functions lox-function
 package zephylix;
 
 import java.util.List;
 
-
-
-public class LoxFunction implements LoxCallable {
-
+class LoxFunction implements LoxCallable {
     private final Stmt.Function declaration;
+    //> closure-field
     private final Environment closure;
 
+    //< closure-field
+/* Functions lox-function < Functions closure-constructor
+  LoxFunction(Stmt.Function declaration) {
+*/
+/* Functions closure-constructor < Classes is-initializer-field
+  LoxFunction(Stmt.Function declaration, Environment closure) {
+*/
+//> Classes is-initializer-field
     private final boolean isInitializer;
 
-   LoxFunction(Stmt.Function declaration){
-       this.declaration = declaration;
-   }
-
-    LoxFunction(Stmt.Function declaration, Environment closure, boolean isInitializer){
+    LoxFunction(Stmt.Function declaration, Environment closure,
+                boolean isInitializer) {
         this.isInitializer = isInitializer;
-         this.declaration = declaration;
-         this.closure = closure;
+//< Classes is-initializer-field
+//> closure-constructor
+        this.closure = closure;
+//< closure-constructor
+        this.declaration = declaration;
     }
-
+    //> Classes bind-instance
     LoxFunction bind(LoxInstance instance) {
         Environment environment = new Environment(closure);
         environment.define("this", instance);
-        return new LoxFunction(declaration, environment, isInitializer);
+/* Classes bind-instance < Classes lox-function-bind-with-initializer
+    return new LoxFunction(declaration, environment);
+*/
+//> lox-function-bind-with-initializer
+        return new LoxFunction(declaration, environment,
+                isInitializer);
+//< lox-function-bind-with-initializer
     }
-   @Override
-    public Object call(Interpreter interpreter, List<Object> arguments){
-       //Environment environment = new Environment(interpreter.globals);
-       Environment environment = new Environment(closure);
-       for (int i = 0; i < declaration.params.size(); i++){
-           environment.define(declaration.params.get(i).lexeme, arguments.get(i));
-       }
-       try {
-           interpreter.executeBlock(declaration.body, environment);
-       } catch (Return returnValue) {
-           if (isInitializer) return closure.getAt(0, "this");
-           return returnValue.value;
-       }
-       if (isInitializer) return closure.getAt(0, "this");
-       return null;
-}
+    //< Classes bind-instance
+//> function-to-string
     @Override
-    public int arity(){
-        return declaration.params.size();
-    }
-    @Override
-    public String toString(){
+    public String toString() {
         return "<fn " + declaration.name.lexeme + ">";
     }
-}
+    //< function-to-string
+//> function-arity
+    @Override
+    public int arity() {
+        return declaration.params.size();
+    }
+    //< function-arity
+//> function-call
+    @Override
+    public Object call(Interpreter interpreter,
+                       List<Object> arguments) {
+/* Functions function-call < Functions call-closure
+    Environment environment = new Environment(interpreter.globals);
+*/
+//> call-closure
+        Environment environment = new Environment(closure);
+//< call-closure
+        for (int i = 0; i < declaration.params.size(); i++) {
+            environment.define(declaration.params.get(i).lexeme,
+                    arguments.get(i));
+        }
 
+/* Functions function-call < Functions catch-return
+    interpreter.executeBlock(declaration.body, environment);
+*/
+//> catch-return
+        try {
+            interpreter.executeBlock(declaration.body, environment);
+        } catch (Return returnValue) {
+//> Classes early-return-this
+            if (isInitializer) return closure.getAt(0, "this");
+
+//< Classes early-return-this
+            return returnValue.value;
+        }
+//< catch-return
+//> Classes return-this
+
+        if (isInitializer) return closure.getAt(0, "this");
+//< Classes return-this
+        return null;
+    }
+//< function-call
+}
--- java/Zephylix/src/zephylix/LoxInstance.java
+++ java/Zephylix/src/zephylix/LoxInstance.java
@@ -1,33 +1,44 @@
+//> Classes lox-instance
 package zephylix;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class LoxInstance {
-    private final LoxClass klass;
+class LoxInstance {
+    private LoxClass klass;
+    //> lox-instance-fields
     private final Map<String, Object> fields = new HashMap<>();
+//< lox-instance-fields
 
     LoxInstance(LoxClass klass) {
         this.klass = klass;
     }
 
+    //> lox-instance-get-property
     Object get(Token name) {
         if (fields.containsKey(name.lexeme)) {
             return fields.get(name.lexeme);
         }
 
-        LoxFunction method = klass.findMethod(this, name.lexeme);
+//> lox-instance-get-method
+        LoxFunction method = klass.findMethod(name.lexeme);
+/* Classes lox-instance-get-method < Classes lox-instance-bind-method
+    if (method != null) return method;
+*/
+//> lox-instance-bind-method
         if (method != null) return method.bind(this);
+//< lox-instance-bind-method
 
-        throw new RuntimeError(name,
+//< lox-instance-get-method
+        throw new RuntimeError(name, // [hidden]
                 "Undefined property '" + name.lexeme + "'.");
     }
-
+    //< lox-instance-get-property
+//> lox-instance-set-property
     void set(Token name, Object value) {
         fields.put(name.lexeme, value);
     }
-
-
+    //< lox-instance-set-property
     @Override
     public String toString() {
         return klass.name + " instance";
