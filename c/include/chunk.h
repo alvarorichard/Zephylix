ca07176bad1133dea138940dfe5ff9e438b0ff58
alvarorichard <alvaro10048@protonmail.com>
2024-07-25 01:31:28 -0300

    update code and fix code C

--- c/include/chunk.h
+++ c/include/chunk.h
@@ -1,3 +1,42 @@
+// //
+// // Created by krone on 4/1/24.
+// //
+
+// #ifndef ZEPHYLIX_CHUNK_H
+// #define ZEPHYLIX_CHUNK_H
+
+// #include "common.h"
+// #include "value.h"
+// #include <stdint.h>
+
+// typedef enum {
+// 	OP_RETURN,
+// 	OP_CONSTANT,
+// 	OP_NEGATE,
+// 	OP_ADD,
+// 	OP_SUBTRACT,
+// 	OP_MULTIPLY,
+// 	OP_DIVIDE,
+// } OpCode;
+
+// typedef struct {
+// 	uint8_t *code;
+// 	int count;
+// 	int capacity;
+// 	int *lines;
+
+// 	ValueArray constants;
+
+// } Chunk;
+
+// void intitChunk(Chunk *chunk);
+// // void writeChunk(Chunk *chunk, uint8_t byte);
+// void freeChunk(Chunk *chunk);
+// int addConstant(Chunk *chunk, Value value);
+// void writeChunk(Chunk *chunk, uint8_t byte, int line);
+
+// #endif // ZEPHYLIX_CHUNK_H
+
 //
 // Created by krone on 4/1/24.
 //
@@ -5,32 +44,29 @@
 #ifndef ZEPHYLIX_CHUNK_H
 #define ZEPHYLIX_CHUNK_H
 
-#include "../include/common.h"
-#include "../include/value.h"
+#include "common.h"
+#include "value.h"
 #include <stdint.h>
 
 typedef enum {
-	OP_RETURN,
-	OP_CONSTANT,
-	OP_NEGATE,
-	OP_ADD,
-	OP_SUBTRACT,
-	OP_MULTIPLY,
-	OP_DIVIDE,
+    OP_RETURN,
+    OP_CONSTANT,
+    OP_NEGATE,
+    OP_ADD,
+    OP_SUBTRACT,
+    OP_MULTIPLY,
+    OP_DIVIDE,
 } OpCode;
 
 typedef struct {
-	uint8_t *code;
-	int count;
-	int capacity;
-	int *lines;
-
-	ValueArray constants;
-
+    uint8_t *code;
+    int count;
+    int capacity;
+    int *lines;
+    ValueArray constants;
 } Chunk;
 
-void intitChunk(Chunk *chunk);
-// void writeChunk(Chunk *chunk, uint8_t byte);
+void initChunk(Chunk *chunk);  // Corrected typo here
 void freeChunk(Chunk *chunk);
 int addConstant(Chunk *chunk, Value value);
 void writeChunk(Chunk *chunk, uint8_t byte, int line);
--- c/sources/main.c
+++ c/sources/main.c
@@ -1,3 +1,117 @@
+// #include "../include/chunk.h"
+// #include "../include/debug.h"
+// #include "../include/vm.h"
+// #include <stdlib.h>
+// #include <stdio.h>
+// #include <string.h>
+
+// #define EXIT_SUCCESS 0
+
+
+// static void repl()
+// {
+//     char line[1024];
+//     for(;;)
+//     {
+//         printf("> ");
+//         if(!fgets(line, sizeof(line), stdin))
+//         {
+//             printf("\n");
+//             break;
+//         }
+//         interpret(line);
+//     }
+// }
+
+// static char* readFile(const char* path)
+// {
+//     FILE* file = fopen(path, "rb");
+//     if(file == NULL)
+//     {
+//         fprintf(stderr, "Could not open file \"%s\".\n", path);
+//         exit(74);
+//     }
+    
+//     fseek(file, 0L, SEEK_END);
+//     size_t fileSize = ftell(file);
+//     rewind(file);
+    
+//     char* buffer = (char*)malloc(fileSize + 1);
+//     if(buffer == NULL)
+//     {
+//         fprintf(stderr, "Not enough memory to read \"%s\".\n", path);
+//         exit(74);
+//     }
+    
+//     size_t bytesRead = fread(buffer, sizeof(char), fileSize, file);
+//     if(bytesRead < fileSize)
+//     {
+//         fprintf(stderr, "Could not read file \"%s\".\n", path);
+//         exit(74);
+//     }
+    
+//     buffer[bytesRead] = '\0';
+    
+//     fclose(file);
+//     return buffer;
+// }
+
+// static void runFile(const char* path)
+// {
+//     char* source = readFile(path);
+//     InterpretResult result = interpret(source);
+//     free(source);
+    
+//     if(result == INTERPRET_COMPILE_ERROR) exit(65);
+//     if(result == INTERPRET_RUNTIME_ERROR) exit(70);
+// }
+
+
+
+
+// int main(int argc, char* argv[])
+// {   
+//     initVM();
+    
+//     if(argc == 1)
+//     {
+//         repl();
+        
+//     } else if(argc == 2)
+//     {
+//         runFile(argv[1]);
+//     } else
+//     {
+//         fprintf(stderr, "Usage: clox [path]\n");
+//         exit(64);
+//     } 
+    
+//     Chunk chunk;
+//     intitChunk(&chunk);  // Corrected typo here
+
+//     int constant = addConstant(&chunk, 1.2);
+//     writeChunk(&chunk, OP_CONSTANT, 123);
+//     constant = addConstant(&chunk, 3.4);
+//     writeChunk(&chunk, OP_CONSTANT, 123);
+//     writeChunk(&chunk, constant, 123);
+   
+//     writeChunk(&chunk, OP_ADD, 123);
+   
+//     constant = addConstant(&chunk, 5.6);
+//     writeChunk(&chunk, OP_CONSTANT, 123);
+//     writeChunk(&chunk, constant, 123);
+   
+//     writeChunk(&chunk, OP_DIVIDE, 123);
+//     writeChunk(&chunk, OP_NEGATE, 123);
+//     writeChunk(&chunk, (uint8_t)constant, 1);  // Pass the constant index
+//     writeChunk(&chunk, OP_RETURN, 123);    
+//     disassembleChunk(&chunk, "test chunk");
+//     interpret(&chunk);
+//     freeVM();
+//     freeChunk(&chunk);
+//     return EXIT_SUCCESS;
+// }
+
 #include "../include/chunk.h"
 #include "../include/debug.h"
 #include "../include/vm.h"
@@ -6,7 +120,6 @@
 #include <string.h>
 
 #define EXIT_SUCCESS 0
-
 
 static void repl()
 {
@@ -66,9 +179,6 @@ static void runFile(const char* path)
     if(result == INTERPRET_RUNTIME_ERROR) exit(70);
 }
 
-
-
-
 int main(int argc, char* argv[])
 {   
     initVM();
@@ -76,37 +186,41 @@ int main(int argc, char* argv[])
     if(argc == 1)
     {
         repl();
-        
-    } else if(argc == 2)
+    }
+    else if(argc == 2)
     {
         runFile(argv[1]);
-    } else
+    }
+    else
     {
         fprintf(stderr, "Usage: clox [path]\n");
         exit(64);
     } 
     
     Chunk chunk;
-    intitChunk(&chunk);  // Corrected typo here
+    initChunk(&chunk);  // Corrected typo here
 
     int constant = addConstant(&chunk, 1.2);
     writeChunk(&chunk, OP_CONSTANT, 123);
     constant = addConstant(&chunk, 3.4);
-     writeChunk(&chunk, OP_CONSTANT, 123);
-     writeChunk(&chunk, constant, 123);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
    
-     writeChunk(&chunk, OP_ADD, 123);
+    writeChunk(&chunk, OP_ADD, 123);
    
-     constant = addConstant(&chunk, 5.6);
-     writeChunk(&chunk, OP_CONSTANT, 123);
-     writeChunk(&chunk, constant, 123);
+    constant = addConstant(&chunk, 5.6);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
    
-     writeChunk(&chunk, OP_DIVIDE, 123);
+    writeChunk(&chunk, OP_DIVIDE, 123);
     writeChunk(&chunk, OP_NEGATE, 123);
     writeChunk(&chunk, (uint8_t)constant, 1);  // Pass the constant index
     writeChunk(&chunk, OP_RETURN, 123);    
     disassembleChunk(&chunk, "test chunk");
-    interpret(&chunk);
+
+    // Removed the incorrect call to interpret
+    // interpret(&chunk);
+
     freeVM();
     freeChunk(&chunk);
     return EXIT_SUCCESS;
