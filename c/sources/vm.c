5d1312e33ee8811c1ff9ad5d7401109cf106df39
alvarorichard <alvaro10048@protonmail.com>
2024-07-30 03:45:20 -0300

    fix object C

--- c/sources/vm.c
+++ c/sources/vm.c
@@ -234,7 +234,7 @@ static InterpretResult run() {
             case OP_FALSE:   push(BOOL_VAL(false)); break;
             case OP_GREATER:  BINARY_OP(BOOL_VAL, >); break;
                   case OP_LESS:     BINARY_OP(BOOL_VAL, <); break;
-            case OP_ADD:      BINARY_OP(NUMBER_VAL, +); break;
+            //case OP_ADD:      BINARY_OP(NUMBER_VAL, +); break;
             case OP_EQUAL: {
                     Value b = pop();
                     Value a = pop();
